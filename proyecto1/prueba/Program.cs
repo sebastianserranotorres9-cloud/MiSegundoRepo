672d74531dfbb860767fe55d922b85b451a3670e
Sebas Serrano <sebastianserranotorres9@gmail.com>
2026-09-01 16:38:13 -0500

    nuevos cambios

--- proyecto1/prueba/Program.cs
+++ proyecto1/prueba/Program.cs
@@ -19,3 +19,5 @@ var connectionString = document.RootElement
 Console.WriteLine("Simulación de cadena de conexión:");
 Console.WriteLine(connectionString ?? "No se encontró la cadena de conexión.");
 
+string stripe_api_key="sk_test_51N0g7cJ8k1Z5Q9z1F"
+
