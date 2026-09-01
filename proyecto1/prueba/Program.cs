e197dd049b8bec32e73a932272ef6debf0c1a829
Sebas Serrano <sebastianserranotorres9@gmail.com>
2026-09-01 00:05:29 -0500

    Commit clase 32

--- proyecto1/prueba/Program.cs
+++ proyecto1/prueba/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, PLATZI");
+﻿using System.Text.Json;
+
+var appSettingsPath = Path.Combine(AppContext.BaseDirectory, "appsettings.json");
+
+if (!File.Exists(appSettingsPath))
+{
+    Console.WriteLine("No se encontró appsettings.json en la carpeta de salida.");
+    return;
+}
+
+using var stream = File.OpenRead(appSettingsPath);
+using var document = JsonDocument.Parse(stream);
+
+var connectionString = document.RootElement
+    .GetProperty("ConnectionStrings")
+    .GetProperty("DefaultConnection")
+    .GetString();
+
+Console.WriteLine("Simulación de cadena de conexión:");
+Console.WriteLine(connectionString ?? "No se encontró la cadena de conexión.");
+
--- proyecto1/prueba/obj/Debug/net10.0/prueba.AssemblyInfo.cs
+++ proyecto1/prueba/obj/Debug/net10.0/prueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+351d1195c826fe90c2d6cdfa0e83734f66fda671")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9954aedaa3749cfaf7b7f2fc09b264aeb7501baf")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
